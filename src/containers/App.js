a84280e98870012ddb5206dfc32a0280fd32d841
ciamac-da <ciamac.davoudi@gmail.com>
2021-03-30 17:16:56 +0200

    Verified by Marti4n v1.0.10

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,54 +2,26 @@ import React, { useState } from "react";
 import "./App.css";
 import UserOutPut from "../components/UserOutput/UserOutput";
 import UserInput from "../components/UserInput/UserInput";
-/* class App extends Component {
-  state = {
-    username: "Cia",
-  };
-
-  usernameChangedHandler = (event) => {
-    this.setState({ username: event.target.value });
-  };
-
-  render() {
-    return (
-      <div className="App">
-        <UserInput
-          changed={this.usernameChangedHandler}
-          currentName={this.state.username}
-        />
-        <UserOutPut userName={this.state.username} />
-        <UserOutPut userName={this.state.username} />
-        <UserOutPut userName={this.state.username} />
-        <UserOutPut userName="Marti4n" />
-      </div>
-    );
-  }
-} */
 
 const App = () => {
+  const [state, setState] = useState("Cia");
 
-  const [state, setState] = useState("Cia")
-  
-  
   const usernameChangedHandler = (event) => {
-    return setState(event.target.value) ;
+    return setState(event.target.value);
   };
 
-
-  return ( <>
-   <div className="App">
-        <UserInput
-          changed={usernameChangedHandler}
-          currentName={state}
-        />
+  return (
+    <>
+      <div className="App">
+        <UserInput changed={usernameChangedHandler} currentName={state} />
         <UserOutPut userName={state} />
         <UserOutPut userName={state} />
         <UserOutPut userName={state} />
         <UserOutPut userName="Marti4n" />
       </div>
-  </> );
-}
- 
+    </>
+  );
+};
+
 export default App;
 
