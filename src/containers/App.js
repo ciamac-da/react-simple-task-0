ab52e4f0122e5f4b4108870001197f8316429ea8
ciamac-da <ciamac.davoudi@gmail.com>
2021-03-30 17:20:22 +0200

    Verified by Marti4n v1.0.11

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,30 +2,8 @@ import React, { useState } from "react";
 import "./App.css";
 import UserOutPut from "../components/UserOutput/UserOutput";
 import UserInput from "../components/UserInput/UserInput";
-/* class App extends Component {
-  state = {
-    username: "Cia",
-  };
 
-  usernameChangedHandler = (event) => {
-    this.setState({ username: event.target.value });
-  };
 
-  render() {
-    return (
-      <div className="App">
-        <UserInput
-          changed={this.usernameChangedHandler}
-          currentName={this.state.username}
-        />
-        <UserOutPut userName={this.state.username} />
-        <UserOutPut userName={this.state.username} />
-        <UserOutPut userName={this.state.username} />
-        <UserOutPut userName="Marti4n" />
-      </div>
-    );
-  }
-} */
 
 const App = () => {
 
