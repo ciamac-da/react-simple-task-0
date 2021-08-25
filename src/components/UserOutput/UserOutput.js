b25b1d5090a562c3a31585395f7f57b5a310baf8
ciamac-da <ciamac.davoudi@gmail.com>
2021-08-25 09:27:53 +0200

    Verified by Marti4n v1.0.2

--- src/components/UserOutput/UserOutput.js
+++ src/components/UserOutput/UserOutput.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../index.css";
 
 const UserOutput = (props) => {
+
+  const [show, setShow] = useState(true)
+
+  if(!show) {
+    return null;
+  }
+
+  const closeBtn = () => {
+    setShow(false)
+  }
+
   return (
     <div className="myDiv">
       <p className="myP">Hi universe!</p>
+      <span className="span" onClick={closeBtn}>X</span>
       <p className="myName">
         Here's:<span className="mySpan">{props.userName}</span>
       </p>
